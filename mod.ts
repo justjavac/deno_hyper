de9a8168ca8323abd9cd85e51edd6bca369e37f2
justjavac <justjavac@gmail.com>
2020-08-23 15:38:05 +0800

    remove unused code

--- mod.ts
+++ mod.ts
@@ -3,25 +3,18 @@ let pluginId = Deno.openPlugin("./target/debug/deno_hyper.dll");
 //@ts-ignore
 const { op_hyper_run } = Deno.core.ops();
 
-//@ts-ignore
-Deno.core.setAsyncHandler(op_hyper_run, (response) => {
-  console.log(`Plugin Async Response: `, response);
-});
-
 export function run(): Promise<void> {
   //@ts-ignore
-  Deno.core.dispatch(
-    op_hyper_run,
-    new Uint8Array([116, 101, 115, 116]),
-    new Uint8Array([49, 50, 51]),
-  )!;
-  
-  return new Promise((r => {
-    //@ts-ignore
-    Deno.core.setAsyncHandler(op_hyper_run, (response) => {
-      r();
-    });
-  }))
+  Deno.core.dispatch(op_hyper_run);
+
+  return new Promise(
+    ((r) => {
+      //@ts-ignore
+      Deno.core.setAsyncHandler(op_hyper_run, (response) => {
+        r();
+      });
+    }),
+  );
 }
 
 export function close(): void {
